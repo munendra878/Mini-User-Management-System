e7d493e9fc1b1a9325d8ba7700095106a7ca41b0
Baghel Munendrasinh <mohitbaghel086@gmail.com>
2025-12-29 18:28:26 +0530

    Initial commit

--- backend/src/api/server.js
+++ backend/src/api/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const app = require("./app");
-const connectDb = require("./src/config/db");
+const app = require("../../app");
+const connectDb = require("../config/db");
 
 const PORT = process.env.PORT || 5000;
 
